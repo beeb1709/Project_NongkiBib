17c6de35efee1f243f86e341884e13928ec66888
Habibie Wibisono <habibiewibisono17@gmail.com>
2026-06-05 11:21:56 +0200

    commit pertama NongkiBib

---
+++ app/src/main/java/com/example/nongkibib/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.nongkibib
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navView: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        // Set default fragment
+        loadFragment(HomeFragment())
+
+        navView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_map -> {
+                    loadFragment(MapFragment())
+                    true
+                }
+                R.id.nav_acara -> {
+                    loadFragment(AcaraFragment())
+                    true
+                }
+                R.id.nav_inbox -> {
+                    loadFragment(InboxFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
+    }
+}
+
+// Simple Fragments classes
+class HomeFragment : Fragment(R.layout.fragment_home)
+class MapFragment : Fragment(R.layout.fragment_map)
+class AcaraFragment : Fragment(R.layout.fragment_acara)
+class InboxFragment : Fragment(R.layout.fragment_inbox)
+class ProfileFragment : Fragment(R.layout.fragment_profile)
